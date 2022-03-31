6425c220089532d9c260bd3df5f1d03ab70ac1fa
Dev-joao-script <dev.joao.script@gmail.com>
2022-04-01 00:17:45 +0100

    Other entities and second migration

--- SalesWebMvc/SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/SalesWebMvc/Models/Department.cs
@@ -1,7 +1,27 @@
-﻿namespace SalesWebMvc.Models {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models {
     public class Department {
         public int id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Department() {
+
+        }
+        public Department(int id, string name) {
+            this.id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller) {
+            this.Sellers.Add(seller);
+        }
+
+        public double TotalSeller(DateTime init, DateTime fin) {
+            return Sellers.Sum(Seller => Seller.TotalSales(init, fin)); 
+        }
     }
 }
